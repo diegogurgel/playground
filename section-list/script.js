e81c0e239f494fc56ec0125870c4ffac853ac060
Diego Gurgel <diego_gurgel@epplkatw0151.budapest.epam.com>
2019-09-04 11:16:35 +0200

    First experiment css sticky list title

---
+++ section-list/script.js
@@ -0,0 +1,15 @@
+// const titles = document.querySelectorAll(".title");
+// const getHandleScroll = elements => ev => {
+//   const target = ev.target;
+//   elements.forEach(element => {
+//     if (target.scrollTop > element.offsetTop) {
+//       element.querySelector("p").classList.add("sticky");
+//     } else {
+//       element.querySelector("p").classList.remove("sticky");
+//     }
+//   });
+// };
+
+// const list = document.querySelector("ul");
+
+// list.addEventListener("scroll", getHandleScroll(titles));
